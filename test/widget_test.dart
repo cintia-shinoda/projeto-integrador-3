2d3ae0c852ade5a99a4d9fb43e651778d2dc1f26
Cintia I. Shinoda <shinoda.c.i@gmail.com>
2025-04-15 21:03:45 -0300

    update: widget_test.dart timer

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,10 +3,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kanji_app/main.dart';
 
 void main() {
-  testWidgets('Verifica se o app carrega corretamente', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+  // testWidgets('Verifica se o app carrega corretamente', (WidgetTester tester) async {
+  //   await tester.pumpWidget(const MyApp());
 
-    // Verifica se existe algum texto ou widget da tela inicial
-    expect(find.byType(MaterialApp), findsOneWidget);
-  });
+  //   // Verifica se existe algum texto ou widget da tela inicial
+  //   expect(find.byType(MaterialApp), findsOneWidget);
+  // });
+  testWidgets('Splash screen navega para login após 2s', (WidgetTester tester) async {
+  await tester.pumpWidget(const MyApp());
+
+  // Aguarda o Future.delayed terminar
+  await tester.pump(const Duration(seconds: 3));
+  await tester.pumpAndSettle();
+
+  expect(find.text('Login'), findsOneWidget); // ou outra verificação adequada
+});
 }
